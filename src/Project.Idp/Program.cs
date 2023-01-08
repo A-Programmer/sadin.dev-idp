12a5b3e8ee829987090cae07a844054c6b6611e1
a-programmer <mrsadin@gmail.com>
2023-01-08 23:54:31 +0330

    Disabled production cert file loader, this should be enabled after fixing issue

--- src/Project.Idp/Program.cs
+++ src/Project.Idp/Program.cs
@@ -20,26 +20,29 @@ else
         .Build();
 }
 
-if (builder.Environment.IsDevelopment())
-{
+// Uncomment this section to use CERT file in production
+// if (builder.Environment.IsDevelopment())
+// {
     builder.Services.AddIdentityServer()
         .AddInMemoryClients(Config.Clients)
         .AddInMemoryApiScopes(Config.ApiScopes)
         .AddInMemoryIdentityResources(Config.IdentityResources)
         .AddTestUsers(Config.TestUsers)
         .AddDeveloperSigningCredential();
-}
-else
-{
-    X509Certificate2 cert = null;
-    cert = new X509Certificate2(Path.Combine(builder.Environment.ContentRootPath, "wwwroot/certs", "aspnetapp.pfx"), "123456");
-    builder.Services.AddIdentityServer()
-        .AddInMemoryClients(Config.Clients)
-        .AddInMemoryApiScopes(Config.ApiScopes)
-        .AddInMemoryIdentityResources(Config.IdentityResources)
-        .AddTestUsers(Config.TestUsers)
-        .AddSigningCredential(cert);
-}
+// }
+// else
+// {
+//     // TODO : this part does not work fine, after deploying I am getting error that can not fine cert file path but
+//     // I think the issue comes from the cert file, I should generate new one and provide the new one
+//     X509Certificate2 cert = null;
+//     cert = new X509Certificate2(Path.Combine(builder.Environment.ContentRootPath, "wwwroot/certs", "aspnetapp.pfx"), "123456");
+//     builder.Services.AddIdentityServer()
+//         .AddInMemoryClients(Config.Clients)
+//         .AddInMemoryApiScopes(Config.ApiScopes)
+//         .AddInMemoryIdentityResources(Config.IdentityResources)
+//         .AddTestUsers(Config.TestUsers)
+//         .AddSigningCredential(cert);
+// }
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
